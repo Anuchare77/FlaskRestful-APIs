a1504213dc5b84ecf3fe06f1d3df53cfc2aa4723
Neelesh Halinge <neelu2025@gmail.com>
2020-09-29 12:17:21 +0200

    Secure file with auth function

---
+++ Flask_app_JWT/security.py
@@ -0,0 +1,15 @@
+from Flask_app_JWT.user import User
+
+users = [User(1,"bob","abcd")]
+username_mapping = {u.username:u for u in users}
+userid_mapping = {u.id:u for u in users}
+
+def authentication(username,password):
+    user = username_mapping.get(username,None)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    user_id =payload['identity']
+    return userid_mapping.get(user_id,None)
+
