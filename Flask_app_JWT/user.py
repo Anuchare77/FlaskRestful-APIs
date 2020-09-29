0e864139ba02c933bb88c151206c676c5b77e37b
Neelesh Halinge <neelu2025@gmail.com>
2020-09-29 12:17:57 +0200

    A user class with id,usrname,pw

---
+++ Flask_app_JWT/user.py
@@ -0,0 +1,9 @@
+# Creating a class user
+
+class User:
+    def __init__(self, _id, username, password):
+        self.id = _id
+        self.username = username
+        self.password = password
+
+
