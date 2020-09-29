4373eed97ce3f6e75e747cbe5ca4be5ac0b460dc
Neelesh Halinge <neelu2025@gmail.com>
2020-09-29 12:52:25 +0200

    Secure file for authentification

---
+++ retreive_itemrsc_fromdb/security.py
@@ -0,0 +1,12 @@
+#contains important functions it has in memory table for user
+from .user import User
+
+#Authenticate user funnctin
+def aunthentication(username,password):
+    user = User.find_by_username(username)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return User.find_by_id(user_id)
