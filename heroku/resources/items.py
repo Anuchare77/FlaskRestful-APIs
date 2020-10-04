7c8fb89bad3c40cc478bb1b1ca3bb53cd040a24e
Anuchare77 <71891383+Anuchare77@users.noreply.github.com>
2020-10-04 17:18:58 +0200

    Update items.py

--- heroku/resources/items.py
+++ heroku/resources/items.py
@@ -1,7 +1,7 @@
 from flask_restful import Resource,reqparse
 from flask_jwt import jwt_required
 import sqlite3
-from heroku_flask.models.items import ItemModel
+from models.items import ItemModel
 
 class Item(Resource):
     parser = reqparse.RequestParser()
