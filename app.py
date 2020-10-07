cf5bfa24e1dc9d6cca1bf0b6af39c3eade25f6d3
Neelesh Halinge <neelu2025@gmail.com>
2020-10-07 19:44:08 +0200

    Added PostgreSQL environmental variable and psycopg2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #Always run create_tables.py file before running app_ex.py file then data.db is created. Initially there will be no data.db file
 #In this case we are retreiving contents of item from database
+import os
 from flask import Flask,request
 from flask_restful import Api
 from flask_jwt import JWT
@@ -12,6 +13,7 @@ from resources.store import StoreList,Store
 from models.store import StoreModel
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL'] = os.environ.get('DATABASE_URL','sqlite:///data.db')   #First argument set in herokupostgre when loading application not from computer but rather from server
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPOGATE_EXCEPTIONS'] = True
